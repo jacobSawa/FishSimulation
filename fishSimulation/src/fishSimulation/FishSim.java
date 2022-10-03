446c38e6875aa22737ec09985e3b57c61be1abee
Jacob <jasawatzky11@gmail.com>
2022-10-03 10:38:02 -0500

    added old stuff back

---
+++ fishSimulation/src/fishSimulation/FishSim.java
@@ -0,0 +1,69 @@
+/*
+ * Jacob Sawatzky, Nicholas Bergen, Carter Reid
+ * October 3, 2022
+ * Fish/Shark Simulation
+ */
+
+package fishSimulation;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.*;
+
+public class FishSim {
+	JFrame frame;
+	JPanel panel;
+	JButton[][] button;
+
+	public FishSim() {
+		frame = new JFrame("FishSim");
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+
+		panel = new JPanel(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		button = new JButton[15][15];
+
+		String[][] ac;
+		
+		for(int i = 0; i < button.length; i ++) {
+			for (int j = 0; j < button[0].length; j ++) {
+				button[i][j].setText("-");
+			}
+		}
+
+		for (int i = 0; i < button.length; i++) {
+			for (int j = 0; j < button[0].length; j++) {
+				//button[i][j].setBorder(null);
+
+				//button[i][j].setActionCommand(ac[i][j]);
+
+				button[i][j].addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						int num1, num2;
+						num1 = Integer.valueOf(e.getActionCommand()) / 10;
+
+						num2 = Integer.valueOf(e.getActionCommand()) % 10;
+
+					}
+
+				});
+
+				c.gridx = i;
+				c.gridy = j;
+				panel.add(button[i][j], c);
+			}
+		}
+
+		frame.setContentPane(panel);
+		frame.setVisible(true);
+	}
+}
---
+++ fishSimulation/src/fishSimulation/Run.java
@@ -0,0 +1,10 @@
+package fishSimulation;
+
+public class Run {
+
+	public static void main(String[] args) {
+		FishSim f = new FishSim();
+
+	}
+
+}

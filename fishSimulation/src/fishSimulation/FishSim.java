fbf6b518aa09aaedb973eeadd2264e9140cc0c13
NicholasBergen <nicbergen472@gmail.com>
2022-10-04 10:05:43 -0500

    image of fortnite

--- fishSimulation/src/fishSimulation/FishSim.java
+++ fishSimulation/src/fishSimulation/FishSim.java
@@ -27,9 +27,9 @@ public class FishSim {
 
 		panel = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		water = new ImageIcon();
+		
 
-		water = new ImageIcon(getClass().getClassLoader().getResource("Deafault.jpg"));
+		water = new ImageIcon(getClass().getClassLoader().getResource("DefaultIcon.jpg"));
 
 		button = new JButton[15][15]; 
 
@@ -38,7 +38,7 @@ public class FishSim {
 
 		for (int i = 0; i < button.length; i++) {
 			for (int j = 0; j < button[0].length; j++) {
-				button[i][j] = new JButton(" - ");
+				button[i][j] = new JButton(water);
 				button[i][j].setBorder(null); 
 
 				//button[i][j].setActionCommand(ac[i][j]);
